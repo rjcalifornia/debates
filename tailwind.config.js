a00ebfcb6863722c055dd9f8c8e029c560cf77fe
rjcalifornia <rodolfohernan20@gmail.com>
2024-11-19 09:50:43 -0600

    Changes to be committed: 	new file:   src/index.html 	new file:   src/input.css 	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}", "./node_modules/preline/preline.js"], // Ensure this covers all the right files
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+    require('@tailwindcss/forms'),
+  ],
 }
 
